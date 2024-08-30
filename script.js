52036c06cf3851bd6b658738a335e8c494a3b4a8
thunder-alish <bajracharya.alish587@gmail.com>
2024-08-30 10:30:25 +0545

    initial commit

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ function prevSlide() {
     slideIndex = (slideIndex - 1 + document.querySelectorAll('.slide').length) % document.querySelectorAll('.slide').length;
     showSlides();
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.counter-number');
+
+    counters.forEach(counter => {
+        const target = counter.getAttribute('data-target');
+        
+        if (counter.innerText !== '65m+') {
+            const updateCounter = () => {
+                const current = parseInt(counter.innerText.replace(/,/g, ''), 10) || 0;
+                const increment = Math.ceil(target / 200);
+
+                if (current < target) {
+                    counter.innerText = formatNumber(current + increment);
+                    setTimeout(updateCounter, 10);
+                } else {
+                    counter.innerText = formatNumber(target);
+                }
+            };
+
+            const formatNumber = (num) => {
+                return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            };
+
+            updateCounter();
+        }
+    });
+});
